672a8d7e61129fa39bc50f09a1baf5cbb9883144
duxingrong <193900684@qq.com>
2024-11-25 10:34:54 +0800

    11-25

---
+++ GrandmaCan_python_opencv-main/tutorial10.py
@@ -0,0 +1,22 @@
+import cv2
+faceCascade = cv2.CascadeClassifier("face_detect.xml")
+
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret , frame = cap.read()
+    if ret:
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faceRect = faceCascade.detectMultiScale(gray, 1.1 , 5)
+        print(faceRect)#看找到了几个人脸，会返回每个人脸的x,y,w,h
+
+        for x,y,w,h in faceRect:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            
+        cv2.imshow('frame',frame)
+    else:
+        break
+    if cv2.waitKey(1) == ord('q'):
+        break
