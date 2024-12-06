33e3f94ab22af2390da28a30ca2526538d94860b
duxingrong <193900684@qq.com>
2024-12-06 23:43:25 +0800

    12-6

---
+++ handtracking/HandTracking/Assets/UDPReceive.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour
+{
+    Thread receiveThread;
+    UdpClient client;
+    public int port = 5052;
+    public bool startRecieving = true;
+    public bool printToConsole = false;
+    public string data;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+    }
+
+
+    // receive thread 
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (startRecieving)
+        {
+            try
+            {
+                IPEndPoint anyIp = new IPEndPoint(IPAddress.Any , 0);
+                byte[] dataByte = client.Receive(ref anyIp);
+                data = Encoding.UTF8.GetString(dataByte);
+
+                if (printToConsole) {print(data);} 
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    
+}
